267c68c185518923ac77920f544908547c81c24c
842156727@qq.com <Wang643174>
2019-09-21 23:29:44 +0800

    日志调试

--- netty-example-getway/src/main/java/com/github/wangzihaogithub/getway/GetwayApplication10002.java
+++ netty-example-getway/src/main/java/com/github/wangzihaogithub/getway/GetwayApplication10002.java
@@ -1,17 +1,23 @@
 package com.github.wangzihaogithub.getway;
 
+import com.github.netty.springboot.EnableNettyEmbedded;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.Route;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
+@EnableNettyEmbedded
 @EnableWebFlux
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GetwayApplication10002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(GetwayApplication10002.class, args);
+        SpringApplication springApplication = new SpringApplication(GetwayApplication10002.class);
+        springApplication.setWebApplicationType(WebApplicationType.REACTIVE);
+        springApplication.run(args);
     }
 
 }
